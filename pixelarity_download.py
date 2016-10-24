5a704e759f73829131db8bd993a74f79a3f6231a
Brian C <bcurtich@gmail.com>
2016-10-24 20:48:52 +0200

    Switched to robobrowser

--- pixelarity_download.py
+++ pixelarity_download.py
@@ -1,23 +1,30 @@
 #!/bin/python3
 
-from bs4 import BeautifulSoup
-import sys
-import urllib3
-import re
+import argparse
 import requests
+from robobrowser import RoboBrowser
 
 def main():
 
-	http = urllib3.PoolManager()
+        parser = argparse.ArgumentParser(description='Login to Pixelarity.')
+        parser.add_argument("email")
+        parser.add_argument("password")
+        args = parser.parse_args()
 
-	r = http.request('GET', 'https://pixelarity.com/')
+        browser = RoboBrowser()
 
-	soup = BeautifulSoup(r.data, 'html.parser')
+        browser.open('https://pixelarity.com/login')
 
-	templates = soup.find("section").find_all("article")
+        login_form = browser.get_form(id='ajaxForm1')
 
-	for index in range(len(templates)):
-		print(templates[index].h2.string)
+        login_form['email'].value = args.email
+        login_form['password'].value = args.password
 
+        browser.submit_form(login_form)
+
+        request = browser.session.get('{{{downloadUrl}}}', stream=True)
+        with open('{{{zipFile}}}', "wb") as temp_zip:
+                temp_zip.write(request.content)
+        
 if __name__ == "__main__":
     main()
